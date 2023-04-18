46bdf8ed23ac96c275a65694e85b9c18c3a04942
Le Tho <thold88@gmail.com>
2023-04-18 14:56:00 +0700

    Get the image model object from wrapped request

--- core/src/main/java/com/adobe/aem/guides/wknd/core/models/impl/BylineImpl.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/impl/BylineImpl.java
@@ -1,8 +1,9 @@
 package com.adobe.aem.guides.wknd.core.models.impl;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
+import javax.annotation.PostConstruct;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
@@ -11,13 +12,11 @@ import org.apache.sling.models.annotations.injectorspecific.OSGiService;
 import org.apache.sling.models.annotations.injectorspecific.Self;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 import org.apache.sling.models.factory.ModelFactory;
-
 import com.adobe.aem.guides.wknd.core.models.Byline;
-import com.day.cq.wcm.foundation.Image;
+import com.adobe.cq.wcm.core.components.models.Image;
 
 @Model(
-        adaptables = {
-                SlingHttpServletRequest.class },
+        adaptables = {SlingHttpServletRequest.class},
         adapters = {Byline.class},
         resourceType = {BylineImpl.RESOURCE_TYPE},
         defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL
@@ -25,26 +24,40 @@ import com.day.cq.wcm.foundation.Image;
 public class BylineImpl implements Byline {
     protected static final String RESOURCE_TYPE = "wknd/components/byline";
 
+    @Self
+    private SlingHttpServletRequest request;
+
     @OSGiService
     private ModelFactory modelFactory;
 
     @ValueMapValue
     private String name;
 
-    @Self
-    private Image image;    
-
-    @Override
-    public String getName() {        
-        return name;
-    }
-
     @ValueMapValue
     private List<String> occupations;
 
+    private Image image;
+
+    /**
+    * @PostConstruct is immediately called after the class has been initialized
+    * but BEFORE any of the other public methods.
+    * It is a good method to initialize variables that is used by methods in the rest of the model
+    *
+    */
+    @PostConstruct
+    private void init() {
+        // set the image object
+        image = modelFactory.getModelFromWrappedRequest(request, request.getResource(), Image.class);
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
     @Override
     public List<String> getOccupations() {
-       if (occupations != null) {
+        if (occupations != null) {
             Collections.sort(occupations);
             return new ArrayList<String>(occupations);
         } else {
@@ -53,16 +66,29 @@ public class BylineImpl implements Byline {
     }
 
     @Override
-    public boolean isEmpty() {        
+    public boolean isEmpty() {
+        final Image componentImage = getImage();
+
         if (StringUtils.isBlank(name)) {
             // Name is missing, but required
             return true;
         } else if (occupations == null || occupations.isEmpty()) {
             // At least one occupation is required
             return true;
+        } else if (componentImage == null || StringUtils.isBlank(componentImage.getSrc())) {
+            // A valid image is required
+            return true;
         } else {
             // Everything is populated, so this component is not considered empty
             return false;
         }
     }
+
+    /**
+    * @return the Image Sling Model of this resource, or null if the resource cannot create a valid Image Sling Model.
+    */
+    private Image getImage() {
+        return image;
+    }
 }
+
