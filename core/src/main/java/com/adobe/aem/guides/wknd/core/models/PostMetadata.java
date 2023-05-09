2a0f53d5846c1eca4248bee0838a7dba2607a77d
Le Tho <thold88@gmail.com>
2023-05-09 16:54:29 +0700

    Add Post Metadata components

---
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/PostMetadata.java
@@ -0,0 +1,9 @@
+package com.adobe.aem.guides.wknd.core.models;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Model;
+
+@Model(adaptables = Resource.class)
+public class PostMetadata {
+
+}
